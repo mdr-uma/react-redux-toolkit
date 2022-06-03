010af37178a0ce6220b8acfda2564de9fc5a55f7
github username <um_2003_8@hotmail.com>
2022-06-03 18:21:04 -0400

    create a state to store count

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { decrement, increment } from './counterSlice'
 
 export function Counter() {
+  const count = useSelector((state) => state.counter.value)
   const dispatch = useDispatch()
 
   return (
